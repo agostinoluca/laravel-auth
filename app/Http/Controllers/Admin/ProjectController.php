4bdcb7e8c6cf24a954165ef9322635ba2cce7f61
LUCA AGOSTINO <agostino.luca89@gmail.com>
2024-06-03 20:52:46 +0200

    Add an else conditional to the ProjectController for the technologies checkboxes

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -101,6 +101,8 @@ class ProjectController extends Controller
 
         if ($request->has('technologies')) {
             $project->technologies()->sync($val_data['technologies']);
+        } else {
+            $project->technologies()->detach();
         }
 
         return to_route('admin.projects.index')->with('status', 'The project was successfully modified!');
