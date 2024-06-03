7d55f28d2097ab4aaebf5ce57d65b51191600a6e
LUCA AGOSTINO <agostino.luca89@gmail.com>
2024-06-03 20:06:20 +0200

    add creation of types and techs using the ProjectSeeder with Faker

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Models\Project;
+use App\Models\Technology;
 use Illuminate\Support\Str;
+use App\Models\Type;
 
 class ProjectSeeder extends Seeder
 {
@@ -15,8 +17,13 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+
+        $types = Type::all()->pluck('id')->all();
+        $technologies = Technology::all()->pluck('id')->all();
+
         for ($i = 0; $i < 5; $i++) {
             $project = new Project();
+            $project->type_id = $faker->randomElement($types);
             $project->title = $faker->words(3, true);
             $project->description = $faker->text(400);
             $project->screenshot_site = $faker->imageUrl(640, 480, 'Develop', true, 'of');
@@ -25,6 +32,8 @@ class ProjectSeeder extends Seeder
             $project->url = $faker->url;
             $project->slug = Str::of($project->title)->slug('-');
             $project->save();
+            $randomTechnologies = $faker->randomElements($technologies, rand(1, 7));
+            $project->technologies()->attach($randomTechnologies);
         }
     }
 }
