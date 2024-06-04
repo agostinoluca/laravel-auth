2c86d91acac241311a2c226fb67380eb6f5f91c7
LUCA AGOSTINO <agostino.luca89@gmail.com>
2024-06-04 21:02:41 +0200

    add function index in Api\ProjectController add new route for all projects in api.php

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        return Project::all();
+    }
 }
